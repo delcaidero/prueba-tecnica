dfcabae4ce8ac2eebf01c5b6808058193b44207a
alby <alby@localhost.localdomain>
2023-02-13 01:01:51 +0000

    build: transform

--- etl/etl/etl.py
+++ etl/etl/etl.py
@@ -11,10 +11,36 @@ from sqlalchemy import create_engine, inspect
 DATASET_URL = "https://api.covidtracking.com/v1/states/daily.csv"
 
 def extract(dataset_url):
-    print(f"Reading dataset from {dataset_url}")
+    """
+    Obtiene el dataset en formato csv y devuelve un dataframe
+    Args:
+    -----
+        dataset_url ([str]): URL del archivo csv a descargar
+    """
+
     df = pd.read_csv(dataset_url)
     return df
+
+def transform(df):
+    """
+    Filtra el dataset y convierte la fecha en datetime.
+    Devuelve modelo con las columnas indicadas
+    Args:
+    -----
+        df ([pandas df]): [Description]]
+    """
+    df_transformed = df.copy()
+    df_transformed = df_transformed[df_transformed.totalTestResultsSource == 'totalTestsViral' ]
+    df_transformed = df_transformed[['date','state','positive','deathConfirmed']]
+    df_transformed['date']= pd.to_datetime(df_transformed['date'], format='%Y%m%d', errors='ignore')
+    return df_transformed
 
 if __name__  == "__main__":
     df_ = extract(DATASET_URL)
     print("carga realizada")
+    print(df_.head(1).to_string())
+
+    clean_df = transform(df_)
+    print("transformacion realizada")
+    print(clean_df.head(1).to_string())
+
