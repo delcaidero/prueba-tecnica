109d6674785e857187e42a5e778115fe92ef3e26
alby <delcaidero@gmail.com>
2023-02-21 00:36:04 +0000

    Build: Informe EDA

--- app/etltools/etltools/etltools.py
+++ app/etltools/etltools/etltools.py
@@ -10,106 +10,139 @@ from functools import wraps
 from sqlalchemy import create_engine, inspect
 
 
-#logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(
-    level=logging.DEBUG, filename="app.log", filemode="w", format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
+class etljob():
+    '''
+    Prueba practica: Script de ETL para el dataset de covidtracking.com
+
+    Crea la conexión a BBDD con los parámetros indicados
+    Extrae los datos del dataset "https://api.covidtracking.com/v1/states/daily.csv"
+    Transforma los datos:
+    - Filtra los registros totalTestResultsSource == "totalTestsViral"
+    - Extrae las columnas: ["date", "state", "positive", "deathConfirmed"]
+    - Convierte "date" a DateTime
+    Crea las siguientes tablas y carga los valores originales y los transformados
 
 
-DATASET_URL = "https://api.covidtracking.com/v1/states/daily.csv"
+    Example:
+    --------
+    import pandas as pd
+    import etltools.etltools as etl
+
+    covidTracking = etljob('docker','docker','postgres','5432')
+    covidTracking.etl()
 
 
-def assign_db_parameters_to(fn):
-    @wraps(fn)
-    def inner(*args, **kwargs):
-        to_execute = fn('docker','docker','postgres','5432')       
-        return to_execute
-    return inner
+    Example:
+    --------
+    python etltools/etltools.py
 
 
-def to_log(fn):
-    @wraps(fn)
-    def inner(*args, **kwargs):
-        logging.debug(f">>> Running {fn.__name__!r} ")
-        to_execute = fn(*args, **kwargs)
-        logging.debug(f">>> Executed {fn.__name__!r} ")
-        return to_execute
-    return inner
+
+    '''
 
 
-@to_log
-def extract(dataset_url):
-    """
-    Obtiene el dataset en formato csv y devuelve un dataframe
-    Args:
-    -----
-        dataset_url ([str]): URL del archivo csv a descargar
-    """
-    df = pd.read_csv(dataset_url)
-    return df
+    def __init__(self, db_user, db_user_pass, db_name, db_port):
+        self.db_user = db_user
+        self.db_user_pass = db_user_pass
+        self.db_name = db_name
+        self.db_port = db_port
+        self.db_table_name_original_data = "original_historic_values"
+        self.db_table_name_transformed_data = "clean_historic_values_all_states"
+        self.dataset_url = "https://api.covidtracking.com/v1/states/daily.csv"
+
+        logging.basicConfig(
+            level=logging.DEBUG, 
+            filename="app.log", 
+            filemode="w", 
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
 
 
-@to_log
-def transform(df):
-    """
-    Filtra el dataset y convierte la fecha en datetime.
-    Devuelve modelo con las columnas indicadas
-    Args:
-    -----
-        df ([pandas df]): [Description]]
-    """
-    df_transformed = df.copy()
-    df_transformed = df_transformed[
-        df_transformed.totalTestResultsSource == "totalTestsViral"
-    ]
-    df_transformed = df_transformed[["date", "state", "positive", "deathConfirmed"]]
-    df_transformed["date"] = pd.to_datetime(
-        df_transformed["date"], format="%Y%m%d", errors="ignore"
-    )
-    return df_transformed
+    def to_log(fn):
+        @wraps(fn)
+        def inner(*args, **kwargs):
+            logging.debug(f">>> Running {fn.__name__!r} ")
+            to_execute = fn(*args, **kwargs)
+            logging.debug(f">>> Executed {fn.__name__!r} ")
+            return to_execute
+        return inner
 
 
-@to_log
-@assign_db_parameters_to
-def connect_db(username,password,host,port):
-    """
-    Creamos la conexión a la BBDD
-    """
-    # postgresql+psycopg2://<username>:<password>@<host>:<port>
-    connection_uri = "postgresql+psycopg2://{}:{}@{}:{}".format(
-        username, password, host, port,
-    )
-    engine = create_engine(connection_uri, pool_pre_ping=True)
-    engine.connect()
-    return engine
+    @to_log
+    def extract(self):
+        """
+        Obtiene el dataset en formato csv y devuelve un dataframe
+        Args:
+        -----
+            dataset_url ([str]): URL del archivo csv a descargar
+        """
+        print("extract...")
+        print(self.dataset_url)
+        df = pd.read_csv(self.dataset_url)
+        return df
 
 
-@to_log
-def load_data_to_db(df, table_name, engine):
-    """
-    Realiza la carga en BBDDD
-    """
-    df.to_sql(table_name, engine, if_exists="append")
+    @to_log
+    def transform(self,df):
+        """
+        Filtra el dataset y convierte la fecha en datetime.
+        Devuelve modelo con las columnas indicadas
+        Args:
+        -----
+            df ([pandas df]): [Description]]
+        """
+        df_transformed = df.copy()
+        df_transformed = df_transformed[
+            df_transformed.totalTestResultsSource == "totalTestsViral"
+        ]
+        df_transformed = df_transformed[["date", "state", "positive", "deathConfirmed"]]
+        df_transformed["date"] = pd.to_datetime(
+            df_transformed["date"], format="%Y%m%d", errors="ignore"
+        )
+        return df_transformed
 
 
-def etl():
-    """
-    Función principal
-    """
-    db_engine = connect_db()
+    @to_log
+    def connect_db(self):
+        """
+        Creamos la conexión a la BBDD
+        """
+        connection_uri = "postgresql+psycopg2://{}:{}@{}:{}".format(
+            self.db_user,
+            self.db_user_pass,
+            self.db_name,
+            self.db_port
+            )
+        engine = create_engine(connection_uri, pool_pre_ping=True)
+        engine.connect()
+        return engine
 
-    original_df = extract(DATASET_URL)
-    original_data_table_name = "original_historic_values"
 
-    clean_df = transform(original_df)
-    clean_table_name = "clean_historic_values_all_states"
-    print(clean_df.head(1).to_string())
+    @to_log
+    def load_data_to_db(self, df, table_name, engine):
+        """
+        Realiza la carga en BBDDD
+        """
+        df.to_sql(table_name, engine, if_exists="append")
 
-    load_data_to_db(original_df, original_data_table_name, db_engine)
-    load_data_to_db(clean_df, clean_table_name, db_engine)
 
-    db_engine.dispose()
+    def etl(self):
+        """
+        Función principal
+        """
+        db_engine = self.connect_db()
+
+        original_df = self.extract()
+
+        clean_df = self.transform(original_df)
+        print(clean_df.head(1).to_string())
+
+        self.load_data_to_db(original_df, self.db_table_name_original_data, db_engine)
+        self.load_data_to_db(clean_df, self.db_table_name_transformed_data, db_engine)
+
+        db_engine.dispose()
 
 
 if __name__ == "__main__":
-    etl()
+    covidTracking = etljob('docker','docker','postgres','5432')
+    covidTracking.etl()
--- app/etltools/tests/test_etltools.py
+++ app/etltools/tests/test_etltools.py
@@ -2,6 +2,9 @@ import etltools.etltools as etl
 import pytest
 from pandas.testing import assert_frame_equal
 
+'''
+Los test están pendientes :(
+'''
 
 TEST_CSV="./tests/test.csv"
 URI_CSV="https://api.covidtracking.com/v1/states/daily.csv"
@@ -25,23 +28,3 @@ def test_extract_expected_columns():
     valid_df = etl.extract(TEST_CSV)
     uri_df = etl.extract(URI_CSV)
     assert_frame_equal(valid_df.head(0), uri_df.head(0), check_dtype=False)
-    
-    
-    '''
-    @pytest.fixture()
-    def database(postgresql):
-        with open('test_db.sql') as f:
-            setup_sql = f.read()
-
-        with postgresql.cursor() as cursor:
-            cursor.execute(setup_sql)
-            postgresql.commit()
-
-        yield postgresql
-
-    def test_example(database):
-        #etl.load_data_to_db(df, table_name, engine)
-        valid_df = etl.extract(TEST_CSV)
-        #etl.load_data_to_db(valid_df, clean_historic_values_all_states, database)
-        assert 1 == 1
-    '''
